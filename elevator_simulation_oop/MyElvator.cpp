55d7eab03327507c6a8514006c7f7f78e8981ee3
Robertccl <1368841907@qq.com>
2018-11-26 21:40:48 +0800

    update ignore

--- elevator_simulation_oop/MyElvator.cpp
+++ elevator_simulation_oop/MyElvator.cpp
@@ -10,3 +10,39 @@ MyElvator::MyElvator()
 MyElvator::~MyElvator()
 {
 }
+
+
+//电梯运行规则
+bool MyElvator::canArrive(int elevatorNum, int floorNum)
+{
+	if (floorNum > 0 && floorNum < 41)
+	{
+		if (elevatorNum == 0 || elevatorNum == 1)
+			return true;
+		else if (elevatorNum == 2 || elevatorNum == 3)
+		{
+			if (floorNum < 2 || floorNum > 24)
+				return true;
+		}
+		else if (elevatorNum == 4 || elevatorNum == 5)
+		{
+			if (floorNum > 0 && floorNum < 26)
+				return true;
+		}
+		else if (elevatorNum == 0 || elevatorNum == 1)
+		{
+			if (floorNum < 2 || floorNum%2 == 0)
+				return true;
+		}
+		else if (elevatorNum == 0 || elevatorNum == 1)
+		{
+			if (floorNum%2 == 1)
+				return true;
+		}
+		else
+			return false;
+	}
+	else
+		return false;
+	return false;
+}
--- elevator_simulation_oop/MyElvator.h
+++ elevator_simulation_oop/MyElvator.h
@@ -4,5 +4,11 @@ class MyElvator
 public:
 	MyElvator();
 	~MyElvator();
+
+
+
+
+	//电梯运行规则
+	bool canArrive(int elevatorNum, int floorNum);
 };
 
